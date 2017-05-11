9fd7ca26b2d6f70f0fa7e114e1a2e9a5631dc3b9
isabolic <sabolic.ivan@gmail.com>
2017-05-11 21:56:19 +0200

    moved node.module to ES6 standards, this closes #4

--- node.server/chat.js
+++ node.server/chat.js
@@ -21,13 +21,13 @@
     const server_ip_address = pjson.server || ip.address();
 
 
-    var app       = express();
-    var server    = http.createServer(app);
-    var io        = socketio(server, { serveClient: false });
-    var users     = [];
+    let app = express();
+    let server = http.createServer(app);
+    let io = socketio(server, { serveClient: false });
+    let users = [];
 
 
-    var wiLogger = new(winston.Logger)({
+    let wiLogger = new(winston.Logger)({
         transports: [
             new(winston.transports.Console)(),
             new(winston.transports.File)({
@@ -45,166 +45,161 @@
 
     app.use("/public", express.static("public"));
 
-    mkdirp("logs", function(err) {
-      if (err) {
-          wiLogger.log("info","err creating log folder " + err);
-      }
+    mkdirp("logs", (err) => {
+        if (err) {
+            wiLogger.log("info", "err creating log folder " + err);
+        }
     });
 
-    server.listen(server_port, server_ip_address, function () {
-      console.log( "Listening on " + server_ip_address + ", server_port " + server_port);
+    server.listen(server_port, server_ip_address, () => {
+        console.log("Listening on " + server_ip_address + ", server_port " + server_port);
     });
 
-    io.on("connection", function (socket) {
-      wiLogger.log("info","connected to socket...");
+    io.on("connection", (socket) => {
+        let emit = (emitCmd, data, socketId) => {
+            wiLogger.log("info", "emitCmd : " + emitCmd);
+            wiLogger.log("info", "data : " + JSON.stringify(data));
+            wiLogger.log("info", "socket.room : " + socket.room);
 
-      socket.room = "room-" + Math.round(new Date().getTime()/1000);
-      socket.join(socket.room);
+            if (socketId !== undefined) {
+                wiLogger.log("info", "socketId : " + socketId);
+                if (socket.room !== undefined) {
+                    socket.in(socket.room).broadcast.to(socketId).emit(emitCmd, data);
+                } else {
+                    io.to(socketId).emit(emitCmd, data);
+                }
+            } else {
 
-      socket.emit("room.name", socket.room);
-      wiLogger.log("info","emit room name..." + socket.room);
-
-
-      var emit = function(emitCmd, data, socketId){
-        wiLogger.log("info","emitCmd : " + emitCmd);
-        wiLogger.log("info","data : " + JSON.stringify(data));
-        wiLogger.log("info","socket.room : " + socket.room);
-
-        if (socketId !== undefined){
-          wiLogger.log("info","socketId : " + socketId);
-          if (socket.room !== undefined){
-              //TEST
-              socket.in(socket.room).broadcast.to(socketId).emit(emitCmd, data);
-          } else {
-              io.to(socketId).emit(emitCmd, data);
-          }
-        } else {
-
-          if (socket.room !== undefined){
-              socket.in(socket.room).broadcast.emit(emitCmd, data);
-          } else {
-              socket.broadcast.emit(emitCmd, data);
-          }
+                if (socket.room !== undefined) {
+                    socket.in(socket.room).broadcast.emit(emitCmd, data);
+                } else {
+                    socket.broadcast.emit(emitCmd, data);
+                }
+            }
         }
-      }
 
-      socket.on("new.message", function (data) {
-        wiLogger.log("info","new message... " + data);
+        wiLogger.log("info", "connected to socket...");
 
-        emit("new.message", {
-          username: socket.username,
-          message: data
+        socket.room = "room-" + Math.round(new Date().getTime() / 1000);
+        socket.join(socket.room);
+
+        socket.emit("room.name", socket.room);
+        wiLogger.log("info", "emit room name..." + socket.room);
+
+        socket.on("new.message", (data) => {
+            wiLogger.log("info", "new message... " + data);
+
+            emit("new.message", {
+                username: socket.username,
+                message: data
+            });
         });
 
-      });
+        socket.on("set.room", (data) => {
+            if (data.room === null || data.room === undefined) {
+                return;
+            }
 
-      socket.on("set.room", function(data) {
-        if(data.room === null || data.room === undefined){
-           return;
-        }
+            if (socket.room !== undefined) {
+                wiLogger.log("info", "leaving room..." + socket.room);
+                socket.leave(socket.room);
+                socket.room = undefined;
+            }
 
-        if(socket.room !== undefined){
-          wiLogger.log("info", "leaving room..." + socket.room);
-          socket.leave(socket.room);
-          socket.room = undefined;
-        }
+            if (data.username !== null || data.username !== undefined) {
+                wiLogger.log("info", "add user " + data.username);
+                socket.username = data.username;
 
-        if (data.username !== null || data.username !== undefined){
-          wiLogger.log("info", "add user " + data.username);
-          socket.username = data.username;
+                emit("user.list", {
+                    users: users
+                }, socket.id);
 
-          emit("user.list", {
-            users: users
-          }, socket.id);
+                users.push(data.username);
+            }
 
-          users.push(data.username);
-        }
+            socket.join(data.room);
+            socket.room = data.room;
+            wiLogger.log("info", "added to room..." + socket.room);
 
-        socket.join(data.room);
-        socket.room = data.room;
-        wiLogger.log("info", "added to room..." + socket.room);
+            if (socket.username !== undefined) {
+                emit("user.joined", {
+                    username: socket.username
+                });
+            }
 
-        if (socket.username !== undefined){
-          emit("user.joined", {
-            username: socket.username
-          });
-        }
-
-      });
-
-      socket.on("public", function(data) {
-        wiLogger.log("info", "public....");
-
-        if(socket.room !== undefined){
-          socket.leave(socket.room);
-          socket.room = undefined;
-        }
-
-        if (data.username !== null && data.username !== undefined){
-          wiLogger.log("info", "add user to public chat " + data.username);
-          socket.username = data.username;
-
-          emit("user.list", {
-            users: users
-          }, socket.id);
-
-          users.push(data.username);
-        }
-
-        if (socket.username !== undefined){
-          emit("user.joined", {
-            username: socket.username
-          });
-        }
-      });
-
-      socket.on("add.user", function (data) {
-
-        if (data.username === null || data.username === undefined){
-          return;
-        }
-
-        wiLogger.log("info", "add user " + data.username);
-        socket.username = data.username;
-        users.push(data.username);
-
-        emit("user.joined", {
-          username: socket.username
         });
 
-      });
+        socket.on("public", (data) => {
+            wiLogger.log("info", "public....");
 
-      socket.on("typing", function () {
-        wiLogger.log("info", socket.username + " is typing...");
+            if (socket.room !== undefined) {
+                socket.leave(socket.room);
+                socket.room = undefined;
+            }
 
-        if (socket.username !== undefined){
-          emit("typing", {
-            username: socket.username
-          });
-        }
-      });
+            if (data.username !== null && data.username !== undefined) {
+                wiLogger.log("info", "add user to public chat " + data.username);
+                socket.username = data.username;
 
-      socket.on("stop.typing", function () {
+                emit("user.list", {
+                    users: users
+                }, socket.id);
 
-        if (socket.username !== undefined){
-          emit("stop.typing", {
-            username: socket.username
-          });
-        }
-      });
+                users.push(data.username);
+            }
 
-      socket.on("disconnect", function () {
-        if (socket.username !== undefined){
-          users = users.filter(function(i) {
-            return !(i === socket.username);
-          });
+            if (socket.username !== undefined) {
+                emit("user.joined", {
+                    username: socket.username
+                });
+            }
+        });
 
-          emit("user.left", {
-            username: socket.username
-          });
+        socket.on("add.user", (data) => {
 
+            if (data.username === null || data.username === undefined) {
+                return;
+            }
 
-        }
-      });
+            wiLogger.log("info", "add user " + data.username);
+            socket.username = data.username;
+            users.push(data.username);
+
+            emit("user.joined", {
+                username: socket.username
+            });
+
+        });
+
+        socket.on("typing", () => {
+            wiLogger.log("info", socket.username + " is typing...");
+
+            if (socket.username !== undefined) {
+                emit("typing", {
+                    username: socket.username
+                });
+            }
+        });
+
+        socket.on("stop.typing", () => {
+
+            if (socket.username !== undefined) {
+                emit("stop.typing", {
+                    username: socket.username
+                });
+            }
+        });
+
+        socket.on("disconnect", () => {
+            if (socket.username !== undefined) {
+                users = users.filter((i) => {
+                    return !(i === socket.username);
+                });
+
+                emit("user.left", {
+                    username: socket.username
+                });
+            }
+        });
     });
 })();
